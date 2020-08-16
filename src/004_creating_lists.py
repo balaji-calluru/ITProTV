b18a30a206ff3f2efaf6fbc1efdba3e32eb13c44
Balaji Calluru <balaji.calluru@gmail.com>
2020-08-15 22:30:08 -0500

    v7

---
+++ src/004_creating_lists.py
@@ -0,0 +1,26 @@
+my_favs = ['Balaji', 'Gubli', 'Adhithya', 'Surya', 'Pranav', 'Madhav', 'Maya', 'Manasa', 'Vasanthi']
+print("Favs: ", my_favs)
+print('3rd one', my_favs[2])
+
+print('Length of my_fav', len(my_favs))
+
+print('Balaji length is', len('Balaji'))
+
+for j in my_favs:
+    print(j, 'length is', len(j))
+
+food = 'fish taco'
+food_list = list(food)
+print(food_list)
+
+nums = [1,2,3,4,5]
+print(nums)
+
+empty = []
+print(empty)
+
+mixed_list = [3, 'Fish', 'Taco', 9.5, False]
+print(mixed_list)
+
+nested_list = [1, 2, 'Boo', [False, 'How']]
+print(nested_list)
