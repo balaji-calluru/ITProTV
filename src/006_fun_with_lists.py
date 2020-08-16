f73d95345e66c7be22d736e52802943815d53eb0
Balaji Calluru <balaji.calluru@gmail.com>
2020-08-15 23:21:42 -0500

    v11

--- src/006_fun_with_lists.py
+++ src/006_fun_with_lists.py
@@ -1,10 +1,18 @@
 my_favs = ['Gubli', 'Maya', 'Manasa', 'Vani', 'Padma', 'Bhuvana', 'Naveena', 'Regina', 'Kavitha']
 my_nums = [6,2,9,4,8,1,7]
 
+# In place Sort and Reverse function... cannot undo the operation... original will be lost
+#
+#print(my_favs)
 #my_favs.sort()
 #print(my_favs)
-
+#my_favs.reverse()
+#print(my_favs)
+#
+#print(my_nums)
 #my_nums.sort()
+#print(my_nums)
+#my_nums.reverse()
 #print(my_nums)
 
 my_sorts = sorted(my_favs)
@@ -22,4 +30,3 @@ my_sorts = sorted(my_nums, reverse=True)
 print(my_sorts)
 my_rev = list(reversed(my_nums))
 print(my_rev)
-
