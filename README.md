5de6cac4ba39544aa8eaf46319dccdef102b888c
Balaji Calluru <balaji.calluru@gmail.com>
2020-08-15 20:54:22 -0500

    V1

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # ITProTV
+
+Learning Python Programming using the tutorial from the ITPro.TV
+
+Follow along
---
+++ src/primitive_data_types.py
@@ -0,0 +1,10 @@
+# this is a comment
+a = 1
+b = True
+print(b)
+while b:
+    print(a)
+    a = a+1
+    if a > 5:
+        b = False
+print(b)
