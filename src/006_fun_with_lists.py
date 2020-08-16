48b0ded57d0f9f3de5d294470b86a72d54f3af21
Balaji Calluru <balaji.calluru@gmail.com>
2020-08-15 23:10:11 -0500

    v9

---
+++ src/006_fun_with_lists.py
@@ -0,0 +1,16 @@
+my_favs = ['Gubli', 'Maya', 'Manasa', 'Vani', 'Padma', 'Bhuvana', 'Naveena', 'Regina', 'Kavitha']
+my_nums = [6,2,9,4,8,1,7]
+
+#my_favs.sort()
+#print(my_favs)
+
+#my_nums.sort()
+#print(my_nums)
+
+my_sorts = sorted(my_favs)
+print(my_favs)
+print(my_sorts)
+
+my_sorts = sorted(my_nums)
+print(my_nums)
+print(my_sorts)
